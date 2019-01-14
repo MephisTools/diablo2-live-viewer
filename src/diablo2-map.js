dc1cd6cc2e17215745f70bf7e528c7358ba3b126
Romain Beaumont <romain.rom1@gmail.com>
2019-01-14 02:56:32 +0100

    display npc and players on map from websocket client

--- src/diablo2-map.js
+++ src/diablo2-map.js
@@ -1,4 +1,4 @@
-/* globals customElements */
+/* globals customElements, WebSocket */
 
 import { LitElement, html } from '@polymer/lit-element/'
 import L from 'leaflet'
@@ -11,49 +11,74 @@ L.Icon.Default.mergeOptions({
   shadowUrl: require('leaflet/dist/images/marker-shadow.png')
 })
 
+function transformCoords ({ x, y }) {
+  return { x: x - 4400, y: -y + 4700 }
+}
+
+const yx = L.latLng
+
+const xy = function (x, y) {
+  if (L.Util.isArray(x)) { // When doing xy([x, y]);
+    return yx(x[1], x[0])
+  }
+  return yx(y, x) // When doing xy(x, y);
+}
+
 class Diablo2Map extends LitElement {
   firstUpdated () {
     this.displayMap()
+    this.entities = {}
   }
 
   displayMap () {
     const mapElement = this.shadowRoot.querySelector('#map')
-    const map = L.map(mapElement, {
+    this.map = L.map(mapElement, {
       crs: L.CRS.Simple,
       minZoom: -3
     })
 
-    const yx = L.latLng
-
-    const xy = function (x, y) {
-      if (L.Util.isArray(x)) { // When doing xy([x, y]);
-        return yx(x[1], x[0])
-      }
-      return yx(y, x) // When doing xy(x, y);
-    }
-
     const bounds = [xy(-25, -26.5), xy(428, 220)]
-    L.imageOverlay('assets/Rogue_Encampment_Map.jpg', bounds).addTo(map)
+    L.imageOverlay('assets/Rogue_Encampment_Map.jpg', bounds).addTo(this.map)
 
-    const sol = xy(175.2, 145.0)
-    xy(41.6, 130.1)
-    const kruegerZ = xy(13.4, 56.5)
-    const deneb = xy(218.7, 8.3)
+    this.map.setView(xy(120, 70), 1)
 
-    const solM = L.marker(sol, { title: 'sol' }).addTo(map).bindTooltip('Test Label',
-      {
-        permanent: true,
-        direction: 'right'
-      })
-    L.marker(kruegerZ).addTo(map).bindPopup('Krueger-Z')
-    L.marker(deneb).addTo(map).bindPopup('Deneb')
+    const ws = new WebSocket('ws://localhost:8080')
 
-    setInterval(() => {
-      console.log(solM.getLatLng())
-      solM.setLatLng(xy(solM.getLatLng().lng + 20 * (Math.random() - 0.5), solM.getLatLng().lat + 20 * (Math.random() - 0.5)))
-    }, 1000)
+    ws.onmessage = (message) => {
+      const { protocol, name, params } = JSON.parse(message.data)
+      console.log(protocol, name, JSON.stringify(params))
 
-    map.setView(xy(120, 70), 1)
+      if (name === 'D2GS_NPCMOVE') {
+        let { x, y, unitId } = params;
+        ({ x, y } = transformCoords({ x, y }))
+        if (this.entities[unitId] === undefined) {
+          this.entities[unitId] = L.marker(xy(x, y)).addTo(this.map).bindTooltip('NPC ' + unitId, { permanent: true, direction: 'right' })
+        } else {
+          this.entities[unitId].setLatLng(xy(x, y))
+        }
+      }
+
+      if (name === 'D2GS_PLAYERMOVE') {
+        let { targetX: x, targetY: y, unitId } = params;
+        ({ x, y } = transformCoords({ x, y }))
+        if (this.entities[unitId] === undefined) {
+          this.entities[unitId] = L.marker(xy(x, y)).addTo(this.map).bindTooltip('player ' + unitId, { permanent: true, direction: 'right' })
+        } else {
+          this.entities[unitId].setLatLng(xy(x, y))
+        }
+      }
+
+      if (name === 'D2GS_WALKVERIFY') {
+        let { x, y } = params;
+        ({ x, y } = transformCoords({ x, y }))
+        const unitId = 99999
+        if (this.entities[unitId] === undefined) {
+          this.entities[unitId] = L.marker(xy(x, y)).addTo(this.map).bindTooltip('myself ', { permanent: true, direction: 'right' })
+        } else {
+          this.entities[unitId].setLatLng(xy(x, y))
+        }
+      }
+    }
   }
 
   render () {
